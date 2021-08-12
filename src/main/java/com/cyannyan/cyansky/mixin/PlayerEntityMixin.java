41e27b465367b8bb06fd7eecf9159d0c9a72cdfb
MiraCrypto <miracrypto@outlook.com>
2021-08-12 23:53:33 +0000

    Update some mechanics

--- src/main/java/com/cyannyan/cyansky/mixin/PlayerEntityMixin.java
+++ src/main/java/com/cyannyan/cyansky/mixin/PlayerEntityMixin.java
@@ -5,8 +5,13 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -20,7 +25,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
   @Shadow public abstract boolean damage(DamageSource source, float amount);
-  
+
+  @Shadow public abstract void sendMessage(Text message, boolean actionBar);
+
   private BlockPos lastSafePos = null;
   
   protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
@@ -29,16 +36,16 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
   @Inject(at = @At("TAIL"), method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", cancellable = true)
   private void onDropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir) {
+    ItemEntity entity = cir.getReturnValue();
+
     if (throwRandomly) {
-      ItemEntity entity = cir.getReturnValue();
-      Vec3d velocity = entity.getVelocity();
-      
-      entity.setVelocity(velocity.multiply(0.1, 1, 0.1));
       entity.setGlowing(true);
-      
-      // Prevent items instantly destroyed
-      if (entity.getY() < -50) {
+      Vec3d velocity = entity.getVelocity();
+      if (getY() < 0) {
+        entity.setVelocity(velocity.multiply(0.7, 1, 0.7));
         entity.setPosition(entity.getX(), 0, entity.getZ());
+      } else {
+        entity.setVelocity(velocity.multiply(0.1, 1, 0.1));
       }
       
       cir.setReturnValue(entity);
@@ -48,25 +55,30 @@ public abstract class PlayerEntityMixin extends LivingEntity {
   @Inject(at = @At("TAIL"), method = "tick()V", cancellable = true)
   private void onTick(CallbackInfo ci) {
     // Save last safe position
-    if (isOnGround()) {
-      BlockPos posDown = getBlockPos().down();
-      BlockState state = world.getBlockState(posDown);
-      if (state.getMaterial().isSolid()) {
-        lastSafePos = getBlockPos();
-      }
+    if (isOnGround() && checkSafe(getBlockPos())) {
+      lastSafePos = getBlockPos();
     }
   }
   
+  private boolean checkSafe(BlockPos pos) {
+    BlockPos posDown = pos.down();
+    BlockState state = world.getBlockState(posDown);
+    return state.getMaterial().isSolid();
+  }
+
   @Inject(at = @At("TAIL"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", cancellable = true)
   private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-    if (cir.getReturnValue() && !isDead() && source.isOutOfWorld() && lastSafePos != null) {
+    if (cir.getReturnValue() && !isDead() && source.isOutOfWorld() && getHealth() < 5 && lastSafePos != null) {
       // Teleport to last safe position
       BlockPos pos = getBlockPos();
       if (Math.abs(pos.getX() - lastSafePos.getX()) + Math.abs(pos.getZ() - lastSafePos.getZ()) < 20) {
         fallDistance = 0;
         this.requestTeleport(lastSafePos.getX(), lastSafePos.getY() + 2, lastSafePos.getZ());
         this.setVelocity(getVelocity().getX(), 0, getVelocity().getZ());
+        Text teleportText = new LiteralText("Be careful! You're teleported back").formatted(Formatting.YELLOW);
+        this.sendMessage(teleportText, false);
       }
     }
   }
+  
 }
